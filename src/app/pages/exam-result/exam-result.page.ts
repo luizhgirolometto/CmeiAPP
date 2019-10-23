6d301a581d2b16b3ac3fa92d8d366fe1db34b177
luizhgirolometto@hotmail.com <Fujilink1018@>
2019-10-23 01:22:41 -0300

    results

--- src/app/pages/exam-result/exam-result.page.ts
+++ src/app/pages/exam-result/exam-result.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExamService } from 'src/app/services/exam.service';
 import { Exam } from 'src/app/models/exam.model';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-exam-result',
@@ -14,7 +15,8 @@ export class ExamResultPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private examService: ExamService) { }
+    private examService: ExamService,
+    public alertController: AlertController) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(data => {
@@ -28,6 +30,36 @@ export class ExamResultPage implements OnInit {
     this.examService.get(key).subscribe(data => {
       this.exam = data;
       this.isLoading = false;
+      console.log('hora do resultado');
+      this.presentAlert();
+      
     })
   }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+ 
+
 }
